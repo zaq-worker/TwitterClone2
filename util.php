73b5d0be68d3f118fd0e94c27d2b05b898133e2f
zaq-worker <yasushi1002-s14@outlook.com>
2021-09-14 20:24:31 +0900

    ログイン機能

--- util.php
+++ util.php
@@ -1,6 +1,4 @@
-
 <?php
-
 /////////////////////////////////////
 //便利な関数
 /////////////////////////////////////
@@ -12,8 +10,7 @@
  * @param string $type user | tweet
  * @return string
  */
-
-function buikdImagePath(string $name = null, string $type)
+function buildImagePath(string $name = null, string $type)
 {
     if ($type === 'user' && !isset($name)) {
         return HOME_URL . 'Views/img/icon-default-user.svg';
@@ -57,4 +54,67 @@ function convertToDayTimeAgo(string $datetime)
     }
 
     return(int)$time . $unix;
+}
+
+/**
+ * ユーザー情報をセッションに保存
+ * 
+ * @param array $user
+ * @return void
+ */
+function saveUserSession(array $user)
+{
+    //セッションを開始していない場合
+    if(session_status()  === PHP_SESSION_NONE) {
+        //セッション開始
+        session_start();
+    }
+
+    $_SESSION['USER'] = $user;
+}
+
+/**
+ * ユーザー情報をセッションから削除
+ * 
+ * @return void
+ */
+function deleteUserSession()
+{
+    //セッションを開始していない場合
+    if (session_status() === PHP_SESSION_NONE) {
+        //セッション開始
+        session_start();
+    }
+
+    //セッションのユーザー情報を削除
+    unset($_SESSION['USER']);
+}
+
+/**
+ * セッションのユーザー情報を取得
+ * 
+ * @return array|false
+ */
+function getUserSession()
+{
+    //セッションを開始していない場合
+    if(session_status()  === PHP_SESSION_NONE) {
+        //セッション開始
+        session_start();
+    }
+
+    if (!isset($_SESSION['USER'])){
+        //セッションにユーザー情報がない
+        return false;
+    }
+
+    $user = $_SESSION['USER'];
+
+    //画像のファイル名からファイルのURLを取得
+    if (!isset($user['image_name'])) {
+        $user['image_name'] = null;
+    }
+    $user['image_path'] = buildImagePath($user['image_name'], 'user');
+
+    return $user;
 }
